8c498d14188aaf1c62962385cd001b2ec3780a1c
ellianacris <ellianacris@hotmail.com>
2022-02-05 15:39:18 -0300

    template base tela

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { useEffect } from "react";
+
+import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
+
 import { getIndicators } from "./services/indicators";
 import { getSimulator } from "./services/simulations";
 
 function App() {
-useEffect(() =>{
-  getIndicators()
-  getSimulator()
-},[])
+  useEffect(() => {
+    getIndicators()
+    getSimulator()
+  }, [])
 
   return (
-    <div >
+    <Container
+      maxWidth="lg"
+      sx={{
+        background: '#efefef',
+        mt: 3,
+        padding: 3
+      }}
+    >
+      <Typography variant="h4" textAlign={"center"} >
+        Simulador de Investimentos
+      </Typography>
 
-    </div>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(12, 1fr)",
+          rowGap: 1,
+          columnGap: 2,
+
+        }}>
+
+
+      </Box>
+    </Container>
   );
 }
 
