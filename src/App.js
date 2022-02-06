f36d2a1173d42f62e1a1984922768a34e272372d
ellianacris <ellianacris@hotmail.com>
2022-02-05 21:03:29 -0300

    theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
-import { Box, Button, ButtonGroup, Container, IconButton, ToggleButton, Typography } from "@mui/material";
+import { Box, Button, Container, IconButton, ToggleButton, Typography } from "@mui/material";
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
+import { getValue } from "@testing-library/user-event/dist/utils";
+import { theme } from './constants/theme'
+import InputRHF from "./components/RHF/InputRHF";
+import { ThemeProvider } from '@mui/material/styles'
 //import { getIndicators } from "./services/indicators";
 //import { getSimulator } from "./services/simulations";
-import InputRHF from "./components/RHF/InputRHF";
-import * as yup from "yup";
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useForm } from "react-hook-form";
-import { getValue } from "@testing-library/user-event/dist/utils";
 
 const mode = 'onSubmit';
 
@@ -61,203 +63,206 @@ function App() {
   }
 
   return (
-    <Container
-      maxWidth="xl"
-      sx={{
-        background: '#efefef',
-        mt: 3,
-        padding: 3
-      }}
-    >
-      <Typography variant="h4" textAlign={"center"} >
-        Simulador de Investimentos
-      </Typography>
+    <ThemeProvider theme={theme}>
 
-      <Box
-        onSubmit={handleSubmit(onSubmit)}
-        autoComplete='off'
-        component='form'
+      <Container
+        maxWidth="xl"
         sx={{
-          display: "grid",
-          gridTemplateColumns: "repeat(12, 1fr)",
-          rowGap: 1,
-          columnGap: 2,
-
-        }}>
+          background: '#efefef',
+          mt: 3,
+          padding: 3
+        }}
+      >
+        <Typography variant="h4" textAlign={"center"} >
+          Simulador de Investimentos
+        </Typography>
 
         <Box
+          onSubmit={handleSubmit(onSubmit)}
+          autoComplete='off'
+          component='form'
           sx={{
             display: "grid",
-            gridTemplateColumns: "repeat(6, 1fr)",
+            gridTemplateColumns: "repeat(12, 1fr)",
             rowGap: 1,
             columnGap: 2,
-            gridColumn: 'span 6'
 
-          }}
-        >
+          }}>
 
-          <Typography
-            variant="h5"
-            textAlign={"left"}
+          <Box
             sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(6, 1fr)",
+              rowGap: 1,
+              columnGap: 2,
               gridColumn: 'span 6'
+
             }}
           >
-            Simulador
-          </Typography>
-
-
-          <Box sx={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)",
-            rowGap: 1,
-            columnGap: 2,
-            gridColumn: 'span 3'
-          }}>
-            <Typography
-              variant="subtitle1"
-              textAlign={"left"}
-              sx={{ gridColumn: 'span 2' }}
-            >
-              Rendimento
-            </Typography>
-
-            <IconButton>
-              <InfoOutlinedIcon />
-            </IconButton>
-          </Box>
-
-          <Box sx={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)",
-            rowGap: 1,
-            columnGap: 2,
-            gridColumn: 'span 3'
-          }}>
 
             <Typography
-              variant="subtitle1"
+              variant="h5"
               textAlign={"left"}
-              sx={{ gridColumn: 'span 2' }}
+              sx={{
+                gridColumn: 'span 6'
+              }}
             >
-              Tipos de Indexação
+              Simulador
             </Typography>
 
-            <IconButton>
-              <InfoOutlinedIcon />
-            </IconButton>
-          </Box>
 
-          <ToggleButtonGroup
-            value={salaryType}
-            exclusive
-            onChange={handleSalaryType}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          >
-            <ToggleButton value="bruto">Bruto</ToggleButton>
-            <ToggleButton value="liquido">Líquido</ToggleButton>
-          </ToggleButtonGroup>
-
-          <ToggleButtonGroup
-            value={incomeType}
-            exclusive
-            onChange={handleIncomeType}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          >
-            <ToggleButton value="pre">PRÉ</ToggleButton>
-            <ToggleButton value="pos">PÓS</ToggleButton>
-            <ToggleButton value="fixado">FIXADO</ToggleButton>
-
-          </ToggleButtonGroup>
-
-          <InputRHF
-            name='initialContribution'
-            label='Aporte Inicial'
-            control={control}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          />
-          <InputRHF
-            name='monthlyContribution'
-            label='Aporte Mensal'
-            control={control}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          />
-          <InputRHF
-            name='period'
-            label='Prazo (em meses)'
-            control={control}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          />
-
-          <InputRHF
-            name='profitability'
-            label='Rentabilidade'
-            control={control}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          />
-
-          <InputRHF
-            name='ipca'
-            label='IPCA (ao ano)'
-            control={control}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          />
-          <InputRHF
-            name='cdi'
-            label='CDI (ao ano)'
-            control={control}
-            sx={{
-              gridColumn: 'span 3'
-            }}
-          />
-
-          <Button
-            variant="outlined"
-            sx={{
+            <Box sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              rowGap: 1,
+              columnGap: 2,
               gridColumn: 'span 3'
             }}>
-            Limpar campos
-          </Button>
-          
-          <Button
-            variant="outlined"
-            sx={{
+              <Typography
+                variant="subtitle1"
+                textAlign={"left"}
+                sx={{ gridColumn: 'span 2' }}
+              >
+                Rendimento
+              </Typography>
+
+              <IconButton>
+                <InfoOutlinedIcon />
+              </IconButton>
+            </Box>
+
+            <Box sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              rowGap: 1,
+              columnGap: 2,
               gridColumn: 'span 3'
+            }}>
+
+              <Typography
+                variant="subtitle1"
+                textAlign={"left"}
+                sx={{ gridColumn: 'span 2' }}
+              >
+                Tipos de Indexação
+              </Typography>
+
+              <IconButton>
+                <InfoOutlinedIcon />
+              </IconButton>
+            </Box>
+
+            <ToggleButtonGroup
+              value={salaryType}
+              exclusive
+              onChange={handleSalaryType}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            >
+              <ToggleButton value="bruto">Bruto</ToggleButton>
+              <ToggleButton value="liquido">Líquido</ToggleButton>
+            </ToggleButtonGroup>
+
+            <ToggleButtonGroup
+              value={incomeType}
+              exclusive
+              onChange={handleIncomeType}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            >
+              <ToggleButton value="pre">PRÉ</ToggleButton>
+              <ToggleButton value="pos">PÓS</ToggleButton>
+              <ToggleButton value="fixado">FIXADO</ToggleButton>
+
+            </ToggleButtonGroup>
+
+            <InputRHF
+              name='initialContribution'
+              label='Aporte Inicial'
+              control={control}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            />
+            <InputRHF
+              name='monthlyContribution'
+              label='Aporte Mensal'
+              control={control}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            />
+            <InputRHF
+              name='period'
+              label='Prazo (em meses)'
+              control={control}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            />
+
+            <InputRHF
+              name='profitability'
+              label='Rentabilidade'
+              control={control}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            />
+
+            <InputRHF
+              name='ipca'
+              label='IPCA (ao ano)'
+              control={control}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            />
+            <InputRHF
+              name='cdi'
+              label='CDI (ao ano)'
+              control={control}
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            />
+
+            <Button
+              variant="contained"
+              sx={{
+                gridColumn: 'span 3'
+              }}>
+              Limpar campos
+            </Button>
+
+            <Button
+              variant="contained"
+              sx={{
+                gridColumn: 'span 3'
+              }}
+            >
+              Simular
+            </Button>
+
+
+          </Box>
+
+          {/* GRAFICO  */}
+          <Box
+            sx={{
+              gridColumn: 'span 6',
+              backgroundColor: '#efe54152'
             }}
           >
-            Simular
-          </Button>
 
 
-        </Box>
-
-        {/* GRAFICO  */}
-        <Box
-          sx={{
-            gridColumn: 'span 6',
-            backgroundColor: '#efe54152'
-          }}
-        >
-
+          </Box>
 
         </Box>
-
-      </Box>
-    </Container>
+      </Container>
+    </ThemeProvider>
   );
 }
 
